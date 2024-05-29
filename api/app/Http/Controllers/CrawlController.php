011c8f92895a53c8432e00fab678175773a0dcd0
kevinhouwerzijl <knhouwerzijl@gmail.com>
2024-05-29 11:30:39 +0200

    Alle websites crawlen
    Alle websites worden gecrawld

--- api/app/Http/Controllers/CrawlController.php
+++ api/app/Http/Controllers/CrawlController.php
@@ -12,12 +12,16 @@ use Illuminate\Http\Request;
 class CrawlController extends Controller
 {
     protected $signature = 'crawl';
+    public function test()
+    {
+        return "test";
+    }
     public function get_category_products()
     {
-        //$url = "https://www.bol.com/nl/nl/l/tv-s/7291/";
-        //$site_name = "bol.com";
-        $url = "https://www.coolblue.nl/televisies/filter?sorteren=best-verkocht";
-        $site_name = "coolblue.nl";
+        $url = "https://www.bol.com/nl/nl/l/tv-s/7291/";
+        $site_name = "bol.com";
+        //$url = "https://www.coolblue.nl/televisies/filter?sorteren=best-verkocht";
+        //$site_name = "coolblue.nl";
         //$url = "https://www.mediamarkt.nl/nl/category/televisies-453.html?sort=salescount+desc";
         //$site_name = "mediamarkt.nl";
         $client = new Client();
@@ -43,19 +47,31 @@ class CrawlController extends Controller
                         $link = $product->filter('a')->attr('href');
                         $title = $product->filter('a.product-title')->text();
                         $raw_price = $product->filter('span[data-test="price"]')->text();
-                        $raw_fraction = $product->filter('[data-test="price-fraction"]')->text();
+                        $raw_fraction = $product->filter('sup[data-test="price-fraction"]')->text();
                         if ($raw_fraction !== "-") {
                             $price = preg_replace('/ /', '.', $raw_price);
                         } else {
                             $price = preg_replace('/[- ]/', '', $raw_price);
                         }
-                        $ids[] = [$dataId, "https://www.bol.com" . $link, $title, $price];
-                        $product = new Product;
-                        $product->name = $title;
-                        $product->site_name = "bol.com";
-                        $product->current_price = $price;
-                        $product->url = "https://www.bol.com" . $link;
-                        //$product->save();
+                        $product_check = Product::where('site_name', $site_name)->where('name', $title)->exists();
+                        if($product_check)
+                        {
+                            $action = "update";
+                            $product = Product::where('site_name', $site_name)->where('name', $title)->first();
+                            $product->current_price = $price;
+                            //$product->update();
+                        }
+                        else
+                        {
+                            $action = "new";
+                            $product = new Product;
+                            $product->name = $title;
+                            $product->site_name = "bol.com";
+                            $product->current_price = $price;
+                            $product->url = "https://www.bol.com" . $link;
+                            //$product->save();
+                        }
+                        $ids[] = [$dataId, "https://www.bol.com" . $link, $title, $price, $action];
                     }
                 } elseif ($site_name == "coolblue.nl") {
                     $ahref = $product->filter('.col--4 .position--relative a');
@@ -67,17 +83,27 @@ class CrawlController extends Controller
                         if (!empty ($dataId)) {
                             $title = $product->filter('h3.color--link')->text();
                             $raw_price = $product->filter('strong.sales-price__current.js-sales-price-current')->text();
-                            //$raw_price = $product->filter('.sales-price__current js-sales-price-current')->text();
-                            //$raw_fraction = $product->filter('[data-test="price-fraction"]')->text();
                             $price = preg_replace('/[^\d]/', '', $raw_price);
-                            $ids[] = [$dataId, "https://www.coolblue.nl" . $href, $title, $price];
-
-                            $product = new Product;
-                            $product->name = $title;
-                            $product->site_name = "coolblue.nl";
-                            $product->url = "https://www.coolblue.nl" . $href;
-                            //$product->save();
+                            $product_check = Product::where('site_name', $site_name)->where('name', $title)->exists();
+                            if($product_check)
+                            {
+                                $action = "update";
+                                $product = Product::where('site_name', $site_name)->where('name', $title)->first();
+                                $product->current_price = $price;
+                                //$product->update();
+                            }
+                            else
+                            {
+                                $action = "new";
+                                $product = new Product;
+                                $product->name = $title;
+                                $product->site_name = "coolblue.nl";
+                                $product->current_price = $price;
+                                $product->url = "https://www.coolblue.nl" . $href;
+                                //$product->save();
+                            }
                         }
+                        $ids[] = [$dataId, "https://www.coolblue.nl" . $href, $title, $price, $action];
                     }
                 } elseif ($site_name == "mediamarkt.nl") {
                     $href_component = $product->filter('a[data-test="mms-product-list-item-link"]');
@@ -87,12 +113,28 @@ class CrawlController extends Controller
                     }
                     if (!empty ($dataId)) {
                         $title = $product->filter('p[data-test="product-title"]')->text();
-                        $ids[] = [$dataId, "https://www.mediamarkt.nl" . $href, $title];
-                        $product = new Product;
-                        $product->name = $title;
-                        $product->site_name = "mediamarkt.nl";
-                        $product->url = "https://www.mediamarkt.nl" . $href;
-                        //$product->save();
+                        $raw_price = $product->filterXPath('//div[@data-test="product-price"]//div[@data-test="mms-price"]//span[contains(@class, "iOrmAX") or contains(@class, "fnhqEi")]')->text();
+                        $price = preg_replace('/[^\d,]/', '', $raw_price);
+                        $price = str_replace(',', '', $price);
+                        $product_check = Product::where('site_name', $site_name)->where('name', $title)->exists();
+                        if($product_check)
+                        {
+                            $action = "update";
+                            $product = Product::where('site_name', $site_name)->where('name', $title)->first();
+                            $product->current_price = $price;
+                            //$product->update();
+                        }
+                        else
+                        {
+                            $action = "new";
+                            $product = new Product;
+                            $product->name = $title;
+                            $product->site_name = "mediamarkt.nl";
+                            $product->current_price = $price;
+                            $product->url = "https://www.mediamarkt.nl" . $href;
+                            //$product->save();
+                        }
+                        $ids[] = [$dataId, "https://www.mediamarkt.nl" . $href, $title, $price, $action];
                     }
                 }
             }
@@ -107,17 +149,33 @@ class CrawlController extends Controller
             $response = $client->request('GET', $product->url);
             $html = $response->getBody()->getContents();
             $crawler = new Crawler($html);
-            $productPrice = $crawler->filter('[data-test="price"]')->text();
-            $productFraction = $crawler->filter('[data-test="price-fraction"]')->text();
             $date = Carbon::now();
-            if ($productFraction !== "-") {
-                $productPrice = preg_replace('/ /', '.', $productPrice);
-            } else {
-                $productPrice = preg_replace('/[- ]/', '', $productPrice);
+            if($product->site_name == "bol.com")
+            {
+                $raw_price = $crawler->filter('span[data-test="price"]')->text();
+                $raw_fraction = $crawler->filter('sup[data-test="price-fraction"]')->text();
+                if ($raw_fraction !== "-") {
+                    $price = preg_replace('/ /', '.', $raw_price);
+                } else {
+                    $price = preg_replace('/[- ]/', '', $raw_price);
+                }
+            }
+            elseif($product->site_name == "coolblue.nl")
+            {
+                $raw_price = $crawler->filter('strong.sales-price__current.js-sales-price-current')->text();
+                $price = preg_replace('/[^\d]/', '', $raw_price);
+            }
+            elseif($product->site_name == "mediamarkt.nl")
+            {
+                $raw_price = $crawler->filterXPath('//div[@data-test="product-price"]//div[@data-test="mms-price"]//span[contains(@class, "iOrmAX") or contains(@class, "fnhqEi")]')->text();
+                $price = preg_replace('/[^\d,]/', '', $raw_price);
+                $price = str_replace(',', '', $price);
             }
             $product_price = new ProductPrice();
-            $product_price->price = $productPrice;
+            $product_price->price = $price;
             $product_price->date = $date;
+            $last_price = ProductPrice::where('product_id', $product->id)->first();
+            
             $product_price->product_id = $product->id;
             $product_price->save();
         }
