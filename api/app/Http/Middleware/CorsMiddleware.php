dd192279bb1be390f9d3aaeb0893a0ac03e13d30
kevinhouwerzijl <knhouwerzijl@gmail.com>
2024-05-30 13:19:28 +0200

    Testing
    Added stuff, dunno

---
+++ api/app/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CorsMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $response = $next($request);
+        
+        // Add CORS headers
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        
+        // If it is an OPTIONS request, return a 200 response
+        if ($request->getMethod() == "OPTIONS") {
+            $response->setStatusCode(200);
+            $response->headers->set('Content-Length', '0');
+        }
+
+        return $response;
+    }
+}
