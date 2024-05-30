138d420038478fd5f8e2dc9264fee7fc77b2988a
kevinhouwerzijl <knhouwerzijl@gmail.com>
2024-05-30 12:07:14 +0200

    Update api.php
    Route aangepast

--- api/routes/api.php
+++ api/routes/api.php
@@ -20,12 +20,11 @@ Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
 
 //Site groep
 Route::group(['middleware' => 'api'], function ($router) {
-    Route::get('home', [ProductController::class, 'homepage'])->name('home');
     Route::group(['prefix' => 'categories'], function ($router) {
         Route::get('products', [CategoryController::class, 'products'])->name('products');
     });
 });
-
+Route::get('home', [ProductController::class, 'homepage'])->name('home');
 Route::get('category', [CrawlController::class, 'get_category_products'])->name('get_category_products');
 Route::get('crawl', [CrawlController::class, 'crawl'])->name('crawl');
 Route::get('test', [ProductController::class, 'test'])->name('test');
