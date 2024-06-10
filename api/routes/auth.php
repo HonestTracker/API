c0c0dee3b2a69da8b6ccf5c1fbeaf732181531b0
kevinhouwerzijl <knhouwerzijl@gmail.com>
2024-06-10 13:36:55 +0200

    Update auth.php
    aaa

--- api/routes/auth.php
+++ api/routes/auth.php
@@ -18,9 +18,9 @@ Route::middleware('guest')->group(function () {
     Route::post('register', [RegisteredUserController::class, 'store']);
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
-                ->name('admin.show_login');
+                ->name('login');
 
-    Route::post('login', [AuthenticatedSessionController::class, 'store'])->name('admin.login');
+    Route::post('login', [AuthenticatedSessionController::class, 'store'])->name('login');
 
     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
                 ->name('password.request');
