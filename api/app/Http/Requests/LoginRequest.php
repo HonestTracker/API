35f21d200d6e9c11cdca6fc21c0f17cfba76933d
kevinhouwerzijl <knhouwerzijl@gmail.com>
2024-06-10 10:20:29 +0200

    Create LoginRequest.php
    test

---
+++ api/app/Http/Requests/LoginRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email|exists:users,email',
+            'password' => 'required',
+        ];
+    }
+    public function messages(): array
+    {
+        return [
+            'email.required' => 'Email is verplicht.',
+            'email.email' => 'Dit is geen geldige email.',
+            'email.exists' => 'Deze email is niet bij ons bekend.',
+            'password.required' => 'Wachtwoord is verplicht',
+        ];
+    }
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'errors' => $validator->errors(),
+            'status' => true
+        ], 422));
+    }
+}
