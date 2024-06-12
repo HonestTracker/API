aade2d63d12b259cf4b68a627f96b658601da9a4
kevinhouwerzijl <knhouwerzijl@gmail.com>
2024-06-12 12:00:36 +0200

    Update web.php
    whah

--- api/routes/web.php
+++ api/routes/web.php
@@ -1,6 +1,5 @@
 <?php
 use App\Http\Controllers\AdminController;
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CrawlController;
@@ -13,9 +12,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Middleware;
 
 Route::middleware('auth')->group(function () {
-    Route::get('/', function () {
-        return redirect()->action([AuthenticatedSessionController::class, 'login']);
-    });
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
