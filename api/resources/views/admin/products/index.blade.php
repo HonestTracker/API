b1df151f1c2d7e2144dfd7744f3136b27442575b
kevinhouwerzijl <knhouwerzijl@gmail.com>
2024-06-14 09:19:24 +0200

    Hotfix two :3
    Made evewy delete fowm have theiw own id fwom the loop so that it deletes the wight thing UwU

--- api/resources/views/admin/products/index.blade.php
+++ api/resources/views/admin/products/index.blade.php
@@ -68,14 +68,14 @@
                                                     d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                                             </svg>
                                         </a>
-                                        <form id="product-delete-form"
+                                        <form id="product-delete-form-{{ $product->id }}"
                                             action="{{ route('admin.products.delete', $product) }}" method="POST"
                                             style="display: none;">
                                             @csrf
                                             @method('delete')
                                         </form>
                                         <button type="submit"
-                                        onclick="if (confirm('Weet je zeker dat je {{ $product->name }} wilt verwijderen?')) { event.preventDefault(); document.getElementById('product-delete-form').submit(); }""
+                                        onclick="if (confirm('Weet je zeker dat je {{ $product->name }} wilt verwijderen?')) { event.preventDefault(); document.getElementById('product-delete-form-{{ $product->id }}').submit(); }""
                                             class="font-medium text-red-600 hover:underline"><svg
                                                 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                                 stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
