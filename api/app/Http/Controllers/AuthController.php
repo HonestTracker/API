438d5ffaea92af54021ff30030f926380c12dee8
kevinhouwerzijl <knhouwerzijl@gmail.com>
2024-06-14 13:41:45 +0200

    Update AuthController.php

--- api/app/Http/Controllers/AuthController.php
+++ api/app/Http/Controllers/AuthController.php
@@ -79,8 +79,11 @@ class AuthController extends \Illuminate\Routing\Controller
         if (!$token = auth('api')->attempt($credentials)) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
-        $token = auth('api')->setTTL(100 * 365 * 24 * 60)->attempt($credentials);
-        return $this->respondWithToken($token);
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth('api')->factory()->setTTL(100 * 365 * 24 * 60),
+        ]);
     }
 
     //Functie voor het uitloggen van een ingelogde gebruiker
