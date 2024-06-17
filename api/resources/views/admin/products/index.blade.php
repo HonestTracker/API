3c47b458958f774686d2d76b14f3147f59ce6d9c
kevinhouwerzijl <knhouwerzijl@gmail.com>
2024-06-17 11:37:27 +0200

    Update index.blade.php
    wah

--- api/resources/views/admin/products/index.blade.php
+++ api/resources/views/admin/products/index.blade.php
@@ -6,7 +6,7 @@
                 Products
             </div>
             <div class="flex">
-                <a href="{{ route('api.crawl') }}"
+                <a href="{{ route('crawl') }}"
                     class="text-white bg-green-500 hover:bg-green-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-3">
                     Crawl prices
                 </a>
