438a00003fb5476940ab0ad9f427834e98857fa0
kevinhouwerzijl <knhouwerzijl@gmail.com>
2024-06-30 14:17:27 +0200

    Update ProductController.php

--- api/app/Http/Controllers/ProductController.php
+++ api/app/Http/Controllers/ProductController.php
@@ -324,12 +324,18 @@ class ProductController extends Controller
         if (!$product) {
             return response()->json(['error' => 'Product not found'], 404);
         }
-        //$similair_products = Product::where('site.category_id', $product->site->category_id)->inRandomOrder()->take(3)->get();
+        $similar_products = Product::whereHas('site', function ($query) use ($product) {
+            $query->where('category_id', $product->site->category_id);
+        })
+        ->inRandomOrder()
+        ->take(3)
+        ->get();
+    
         $user = Auth::user();
 
         return response()->json([
             'user' => $user,
-            //'similair_products' => $similair_products,
+            'similair_products' => $similar_products,
             'product' => $product,
         ]);
     }
