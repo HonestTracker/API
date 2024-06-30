e935ab420d6e4f6f77a36376921aa641649bd579
kevinhouwerzijl <knhouwerzijl@gmail.com>
2024-06-30 17:23:46 +0200

    Update FavoriteController.php

--- api/app/Http/Controllers/FavoriteController.php
+++ api/app/Http/Controllers/FavoriteController.php
@@ -19,7 +19,6 @@ class FavoriteController extends Controller
     }
     public function store(Request $request)
     {
-        return response()->json($request->all());
         $user = auth()->user();
         $product = Product::where('id', $request->product_id)->first();
         $favorite = new Favorite;
