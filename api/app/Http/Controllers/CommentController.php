c99d3bd86b285bc1a12f92710be9f63736d90ef7
kevinhouwerzijl <knhouwerzijl@gmail.com>
2024-06-30 14:56:05 +0200

    Update CommentController.php

--- api/app/Http/Controllers/CommentController.php
+++ api/app/Http/Controllers/CommentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
 use App\Models\Product;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -12,6 +13,12 @@ class CommentController extends Controller
     {
        $user = User::where('id', $request->user_id)->first();
        $product = Product::where('id', $request->product_id)->first();
-       return response()->json([$user, $product]);
+       $comment = new Comment();
+       $comment->user_id = $user->id;
+       $comment->product_id = $product->id;
+       $comment->text = $request->text;
+       $comment->stars = $request->rating;
+       $comment->save();
+       return response()->json("Saved!");
     }
 }
