f1856da84225239f0dbc2f22e489fbb5d1ee903b
kevinhouwerzijl <knhouwerzijl@gmail.com>
2024-06-30 20:11:29 +0200

    Update CommentController.php

--- api/app/Http/Controllers/CommentController.php
+++ api/app/Http/Controllers/CommentController.php
@@ -10,20 +10,22 @@ use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    public function store(StoreCommentRequest $request)
-    {
-       $user = User::where('id', $request->user_id)->first();
-       $product = Product::where('id', $request->product_id)->first();
-       $comment = new Comment();
-       $comment->user_id = $user->id;
-       $comment->product_id = $product->id;
-       $comment->text = $request->text;
-       $comment->stars = $request->stars;
-       $comment->save();
-       return response()->json("Saved!");
-    }
-    public function delete(Request $request)
-    {
-       return response()->json($request->all());
-    }
+   public function store(StoreCommentRequest $request)
+   {
+      $user = User::where('id', $request->user_id)->first();
+      $product = Product::where('id', $request->product_id)->first();
+      $comment = new Comment();
+      $comment->user_id = $user->id;
+      $comment->product_id = $product->id;
+      $comment->text = $request->text;
+      $comment->stars = $request->stars;
+      $comment->save();
+      return response()->json("Saved!");
+   }
+   public function delete(Request $request)
+   {
+      $comment = Comment::where('id', $request->comment_id)->first();
+      $comment->delete();
+      return response()->json("Deleted!");
+   }
 }
